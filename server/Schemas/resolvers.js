40c7223c6095770ffe4d81cee0885d20aa173735
Daniel Park <danielapark87@gmail.com>
2024-01-15 12:02:28 -0600

    resolvers and typeDefs updated

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,5 +1,33 @@
 const { Book, User } = require ('../models');
 
-const resolvers = {};
+const resolvers = {
+    Query: {
+        user: async (parent, args) => {
+            return User.findById(args.id)
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        },
+        saveBook: async (parent, args, book) => {
+            const user = await User.findByIdAndUpdate(args._id, {
+                $push: {book: args.bookId}
+            }, { new: true});
+            return user
+
+        },
+        removeBook: async (parent, args, book) => {
+            const user = await User.findByIdAndUpdate(args._id, {
+                $pull: {book: args.bookId }
+            }, {new: true})
+            return user;
+        }, 
+        login: async () => {
+
+        },
+    },
+};
 
 module.exports = resolvers;
--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,3 +1,37 @@
-const typeDefs = ``;
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    type Book {
+        bookId: ID!
+        authors: [String]!
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me(_id: String): [User]
+    }
+
+    type Mutation {
+        login (email: String!, password: String!): Auth
+        addUser (username: String!, email: String!, password: String!): Auth
+        saveBook (bookId: ID!, authors: [String]!, description: string, title: String!, image: String, link: String): User
+        removeBook (bookId: ID!): User 
+    }
+
+`;
 
 module.exports = typeDefs;
